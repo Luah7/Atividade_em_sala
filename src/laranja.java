eeec5b4a5698538616fd4a169db19495e3c9bb50
luah7 <luanmachadoo365@gmail.com.br>
2023-02-24 21:27:50 -0300

    trabalho em sala Dia 24/02/2023

---
+++ src/laranja.java
@@ -0,0 +1,16 @@
+
+public class laranja {
+	
+	public static void main(String[] args) {
+		/** joao tem 60 laranjas e gsotaria de dividi-las em quantidades iguais para seus amigos
+		 * Pedro, Mateus e Lucas
+		 */
+		
+		int laranjas = 60;
+		int amigos = 4;
+		int resultado = laranjas / amigos;
+		
+		System.out.println("a quantidade de laranjas é:" + resultado);;
+	}
+}
+
